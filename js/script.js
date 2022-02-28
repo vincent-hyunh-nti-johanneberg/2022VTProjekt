4a1a7865f4c0be32b714737e0dafd732cb46b844
vincent-hyunh-nti-johanneberg <94617501+vincent-hyunh-nti-johanneberg@users.noreply.github.com>
2022-02-28 10:04:25 +0100

    Blev ungefär klar med Nav
    Måste fixa eftersom den fungerar inte

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navlink')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('burger')
+}
+
+burger.addEventListener ('click', toggleMeny)
+nav.addEventListener('click', toggleMeny)
